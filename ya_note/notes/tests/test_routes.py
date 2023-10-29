8319efc1bbfcd9993ff05ac7fe22024a681b616d
Espo <notjustespo@yandex.ru>
2023-10-29 12:40:41 +0300

    PEP8 CHANGES

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -1,10 +1,10 @@
 from http import HTTPStatus
 
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from notes.models import Note
 
-from django.urls import reverse
+from notes.models import Note
 
 User = get_user_model()
 
