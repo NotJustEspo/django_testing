b1e4739ed70dc5d13b5c0f39815120b4bdb6454d
Espo <notjustespo@yandex.ru>
2023-10-29 12:59:56 +0300

    Changes like PEP8 for docstrings

--- ya_news/news/pytest_tests.py/test_routes.py
+++ ya_news/news/pytest_tests.py/test_routes.py
@@ -21,6 +21,7 @@ def test_home_availability_for_anonymous_user(client, name, expected_status):
     """Тестирование доступности главной страницы
     анонимному пользователю
     """
+
     url = reverse(name)
     response = client.get(url)
     assert response.status_code == expected_status
@@ -36,6 +37,7 @@ def test_detail_page_availability_for_anonymous_user(client, news):
 
 
 @pytest.mark.parametrize(
+
     'parametrize_client, expected_status',
     (
         (pytest.lazy_fixture('admin_client'), HTTPStatus.NOT_FOUND),
@@ -55,6 +57,7 @@ def test_availability_for_comment_edit_and_delete(
     """Тестирование удаление и реадктирования комментария
     автору комментария.
     """
+
     url = reverse(name, args=(comment.id,))
     response = parametrize_client.get(url)
     assert response.status_code == expected_status
