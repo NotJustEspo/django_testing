a3349a05c31e8e977c7def9eee7561a9810b4129
Espo <notjustespo@yandex.ru>
2023-10-30 11:05:58 +0300

    Added urls variables in yanote test_routes.py (review-1)

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -19,52 +19,60 @@ class TestRoutes(TestCase):
             text='Текст',
             author=cls.author,
             slug='slug')
+        cls.home_url = reverse('notes:home', None)
+        cls.login_url = reverse('users:login', None)
+        cls.logout_url = reverse('users:logout', None)
+        cls.signup_url = reverse('users:signup', None)
+        cls.add_url = reverse('notes:add', None)
+        cls.list_url = reverse('notes:list', None)
+        cls.success_url = reverse('notes:success', None)
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+        cls.detail_url = reverse('notes:detail', args=(cls.note.slug,))
+        cls.delete_url = reverse('notes:delete', args=(cls.note.slug,))
+
 
     def test_pages_availability(self):
         """Тест отображения страниц для анонимного пользователя"""
         urls = (
-            ('notes:home', None),
-            ('users:login', None),
-            ('users:logout', None),
-            ('users:signup', None),
+            (self.home_url),
+            (self.login_url),
+            (self.logout_url),
+            (self.signup_url),
         )
-        for name, args in urls:
-            with self.subTest(name=name):
-                url = reverse(name, args=args)
-                response = self.client.get(url)
+        for elem in urls:
+            with self.subTest(elem=elem):
+                response = self.client.get(elem)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_availability_for_author(self):
         """Тестирование доступности страниц для авторизованного пользователя"""
         urls = (
-            ('notes:edit', (self.note.slug,)),
-            ('notes:detail', (self.note.slug,)),
-            ('notes:delete', (self.note.slug,)),
-            ('notes:add', None),
-            ('notes:list', None),
-            ('notes:success', None),
+            (self.edit_url),
+            (self.detail_url),
+            (self.delete_url),
+            (self.add_url),
+            (self.list_url),
+            (self.success_url),
         )
-        for name, args in urls:
+        for elem in urls:
             user = self.author
             self.client.force_login(user)
-            with self.subTest(user=user, name=name):
-                url = reverse(name, args=args)
-                response = self.client.get(url)
+            with self.subTest(user=user, elem=elem):
+                response = self.client.get(elem)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_redirect_for_author(self):
         """Тестирование страницы редиректа для неавторизованного автора"""
         login_url = reverse('users:login')
         urls = (
-            ('notes:add', None),
-            ('notes:list', None),
-            ('notes:success', None),
-            ('notes:detail', (self.note.slug,)),
-            ('notes:edit', (self.note.slug,)),
-            ('notes:delete', (self.note.slug,)),
+            (self.add_url),
+            (self.list_url),
+            (self.success_url),
+            (self.detail_url),
+            (self.edit_url),
+            (self.delete_url),
         )
-        for name, args in urls:
-            url = reverse(name, args=args)
-            redirect_url = f'{login_url}?next={url}'
-            response = self.client.get(url)
+        for elem in urls:
+            redirect_url = f'{login_url}?next={elem}'
+            response = self.client.get(elem)
             self.assertRedirects(response, redirect_url)
